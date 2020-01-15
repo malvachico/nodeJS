3d34148f054abf4b254ba2d03451d6c9be91323c
Hamed Sharafi <hamed.sharafi@gmail.com>
2020-01-15 18:23:35 +0330

    simple routing and show dataObject to browser.

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const fs = require('fs');
 const url = require('url');
 const http = require('http');
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, `utf-8`);
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const path = req.url;
   if (path === '/' || path === '/overview') {
     res.end('overview');
   } else if (path === '/product') {
     res.end('product');
+  } else if (path === '/api') {
+    res.writeHead(200, {
+      'content-type': 'application/json'
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'content-type': 'text/html'

b68450ae9f72e347bd1b4c5f5b99db9236917a0e
Hamed Sharafi <hamed.sharafi@gmail.com>
2020-01-15 18:26:54 +0330

    make prettier config file

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, `utf-8`);
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const path = req.url;
-  if (path === '/' || path === '/overview') {
-    res.end('overview');
-  } else if (path === '/product') {
-    res.end('product');
-  } else if (path === '/api') {
-    res.writeHead(200, {
-      'content-type': 'application/json'
-    });
-    res.end(data);
-  } else {
-    res.writeHead(404, {
-      'content-type': 'text/html'
-    });
-    res.end('<h1>page not found!</h1>');
-  }
+    const path = req.url;
+    if (path === '/' || path === '/overview') {
+        res.end('overview');
+    } else if (path === '/product') {
+        res.end('product');
+    } else if (path === '/api') {
+        res.writeHead(200, {
+            'content-type': 'application/json'
+        });
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            'content-type': 'text/html'
+        });
+        res.end('<h1>page not found!</h1>');
+    }
 });
 
 server.listen('8000', '127.0.0.1', () => {
-  console.log('server started on port 8000!');
+    console.log('server started on port 8000!');
 });
